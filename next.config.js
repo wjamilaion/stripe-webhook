e4c668e183967817704ab18019055c90927c37ff
Waqas Jamil <waqas_lahore@yahoo.com>
2025-01-29 21:31:14 +0500

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+        stackbitPreview: process.env.STACKBIT_PREVIEW
+    },
+    trailingSlash: true,
+    reactStrictMode: true
+};
+
+module.exports = nextConfig;
---
+++ src/components/molecules/FormBlock/CheckboxFormControl/index.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import classNames from 'classnames';
+
+export default function CheckboxFormControl(props) {
+    const { name, label, isRequired, width = 'full' } = props;
+    const labelId = `${name}-label`;
+    const attr: any = {};
+    if (label) {
+        attr['aria-labelledby'] = labelId;
+    }
+    if (isRequired) {
+        attr.required = true;
+    }
+    return (
+        <div className={classNames('sb-form-control', 'flex', 'items-center', { 'sb-form-control-full': width === 'full' })}>
+            <input id={name} className="sb-checkbox" type="checkbox" name={name} {...attr} />
+            {label && (
+                <label id={labelId} className="sb-label" htmlFor={name}>
+                    {label}
+                </label>
+            )}
+        </div>
+    );
+}
